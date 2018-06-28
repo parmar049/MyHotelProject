4143ccaf3044dd70ae548f1397e034054057a088
karan <karan@192.168.1.4>
2018-06-28 09:55:17 +0530

    changes for hibernate configuration

--- MyHotelProject/src/main/java/com/parmar/SpringMVC/serviceImpl/UserServiceImpl.java
+++ MyHotelProject/src/main/java/com/parmar/SpringMVC/serviceImpl/UserServiceImpl.java
@@ -2,25 +2,28 @@ package com.parmar.SpringMVC.serviceImpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.parmar.SpringMVC.dao.UserDAO;
 import com.parmar.SpringMVC.model.User;
 import com.parmar.SpringMVC.service.UserService;
 @Service
-
+@Transactional
 public class UserServiceImpl implements UserService{
 	@Autowired
 	private UserDAO userDAO;
+	@Transactional
 	public User getUser(String loginId) {
 		return userDAO.getUser(loginId);
 	}
-
+	@Transactional
 	public boolean validateUser(String loginId, String password) {
 		return userDAO.validateUser(loginId, password);
 	}
-
+	@Transactional
 	public User getUserDetailsAfterLogin(String loginId,String password) {
 		boolean isValidUser=userDAO.validateUser(loginId, password);
+		System.out.println("User Validated :"+isValidUser);
 		if(isValidUser)
 			return userDAO.getUser(loginId);
 		else 
